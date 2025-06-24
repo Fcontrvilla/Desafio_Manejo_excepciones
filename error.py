44762584f3b0f1cb67ccaa9cc42b2e20e1d0faee
Fcontrvilla <fcontreras@gmail.com>
2025-06-24 00:58:39 +0000

    1 ok

---
+++ error.py
@@ -0,0 +1,22 @@
+
+
+class DimensionError(Exception):
+    def __init__(self, mensaje, dimension, maximo):
+        self.mensaje = mensaje
+        self.dimension = dimension
+        self.maximo = maximo
+
+
+    """En la misma clase anterior, sobrecargar el método __str__, de forma tal que si sólo
+se ha ingresado mensaje al crear la excepción, se retorna el método de la clase padre.
+En caso contrario, crear y retornar un mensaje de retorno utilizando los atributos
+mensaje y/o dimension y/o maximo."""
+    
+    def __str__(self)     #sobrecarga metodo
+
+        if self.dimesion is None and self.maximo is None:
+            return super().__str__(Exception)
+        
+        elif
+        else:
+            return f"se ingresó {self.mensaje}, {self.dimension}, {self.maximo}"
